602977fde1cc2e68fd925d9d2a073968939bf0b0
Neilson Venute <neilsonvenute@gmail.com>
2022-04-13 20:16:51 -0300

    firt commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'import/prefer-default-export': 'off',
+  },
+};
---
+++ src/pages/login/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from '../../styles/GlobalStyles';
+import { Paragrafo, Title } from './styled';
+
+export default function Login() {
+  return (
+    <Container>
+      <Title>
+        Teste
+        <small>Oie</small>
+      </Title>
+      <Paragrafo>Loren ipsun dolar sit amet.</Paragrafo>
+    </Container>
+  );
+}
